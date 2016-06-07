62d4b0561241e7be3ba10e3c5f0d204aa3301f74
Spoonflower <mhowell@spoonflower.com>
2016-06-07 14:56:27 -0400

    almost done with script1

--- script1.rb
+++ script1.rb
@@ -3,7 +3,6 @@ require "./lib/all"
 require 'pry'
 
 puts "How many items are there? #{Item.count}"
-
 #----------------------
 
 most_exspensive_item = Item.new(price: 0.0)
@@ -14,23 +13,31 @@ Item.all.each do |i|
   end
 end
 puts "What is the most expensive item? #{most_exspensive_item.description}"
-
 #----------------------
 
 address = Address.where(street: "7153 Predovic Falls")
 address.first.user_id
 user = User.find(96)
 puts "#{user.first_name} #{user.last_name} lives at 7153 Predovic Falls?"
-
 #----------------------
 
+bottle = Item.find_by(description: "Mediocre Copper Bottle")
+sold = bottle.purchases.map{ |p| p.quantity}.reduce(:+)
+puts "How many Mediocre Copper Bottles did we sell? #{sold} "
+#---------------------
+
+purchases = Purchase.all
+total_revenue = purchases.map { |p| (p.item.price.to_f)*(p.quantity)}.reduce(:+)
+
+puts "What is our total revenue was $#{total_revenue.round(2)}."
+#--------------------
+
+all_users = User.all
+carmelo = User.find_by(first_name:"Carmelo",last_name: "Towne")
+spent = Purchase.where(user_id: carmelo.id).map { |p| (p.item.price.to_f)*(p.quantity)}.reduce(:+)
+puts "Carmelo Towne spent $#{spent}."
+#--------------------
 
 
-
-
-puts "How many Mediocre Copper Bottles did we sell? #{} "
-
-
-# What is our total revenue (item cost * quantity sold for all purchases)?
-# How much did Carmelo Towne spend?
+#all_users
 # How many users have > 1 address?
